6fcc00be79de1fb7c0ba342ecb68c93ec08171b6
Cool Developer <cool199966@outlook.com>
2022-09-15 17:53:05 -0400

    add benchmark

--- dmap_test.go
+++ dmap_test.go
@@ -4,15 +4,37 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/cool-develope/deterministic-go-map/dmap"
+	dmap "github.com/cool-develope/deterministic-go-map"
 )
 
 func TestDMap(t *testing.T) {
-	m := dmap.NewMap[string, int]
+	m := dmap.NewMap[string, int]()
 	m.Set("haha", 4)
 	m.Set("hello", 5)
 
 	v, found := m.Get("haha")
-	fmt.Print(v, found)
-	fmt.Print(m.Range())
+	t.Log(v, found)
+	t.Log(m.Range())
+}
+
+const count = 1000000
+
+func BenchmarkDMapSet(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	m := dmap.NewMap[string, int]()
+	for i := 0; i < count; i++ {
+		m.Set(fmt.Sprintf("key%d", i), i)
+	}
+}
+
+func BenchmarkMapSet(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	m := make(map[string]int)
+	for i := 0; i < count; i++ {
+		m[fmt.Sprintf("key%d", i)] = i
+	}
 }
